14335e16b8973f862721fca4d97d8a53c2aeaf75
Sergio Vega <svegaxmr@gmail.com>
2018-06-16 21:24:57 -0500

    Move to module system

---
+++ src/com/svega/crypto/ed25519/Version.kt
@@ -0,0 +1,12 @@
+package com.svega.crypto.ed25519
+
+import com.svega.common.version.Extra
+import com.svega.common.version.Version
+
+class Version: Version(1, 0, 0,
+        makeExtra(Extra.STABLE, 1)){
+    init {
+        com.svega.common.version.Version.requires("com.svega.common", 0, 2)
+        com.svega.common.version.Version.requires("com.svega.crypto.common", 0, 1)
+    }
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module jvm.ed25519 {
     requires kotlin.stdlib;
     requires jvm.crypto;
+    requires svega.common.utils;
 
     exports com.svega.crypto.ed25519;
     exports com.svega.crypto.ed25519.objects;
